c4b3351488403ba33ee50a14fdd370638a979612
EU\kreddy <kotlarishikesh_reddy@trimble.com>
2025-09-15 00:03:10 +0530

    Adding the Scrolling Behaviour To the Website

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.less'
 })
 export class Projects {
+
+  items = ['Card 1', 'Card 2', 'Card 3', 'Card 4', 'Card 5'];
+
+scrollLeft(slider: HTMLElement) {
+  slider.scrollBy({ left: -200, behavior: 'smooth' });
+}
+
+scrollRight(slider: HTMLElement) {
+  slider.scrollBy({ left: 200, behavior: 'smooth' });
+}
 
 }
